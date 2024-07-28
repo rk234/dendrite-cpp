7810533e520ec3a6f80c0b0ebb888111e56aae7f
Ramy Kaddouri <myramkadd@gmail.com>
2024-07-28 21:28:43 +0100

    dataset parsing work

---
+++ src/testing/Mnist.hpp
@@ -0,0 +1,24 @@
+#ifndef MNIST_H
+#define MNIST_H
+#include "math/Matrix.hpp"
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <optional>
+class Mnist {
+private:
+  std::optional<Matrix> trainImages;
+  std::optional<Matrix> trainLabels;
+  std::optional<Matrix> testImages;
+  std::optional<Matrix> testLabels;
+
+  Matrix loadImages(std::filesystem::path file) {
+    std::ifstream stream(file, std::ios_base::binary);
+    uint32_t magicNum;
+    stream >> magicNum;
+  }
+
+public:
+  void load(std::filesystem::path directory) {}
+};
+#endif // !MNIST_H
